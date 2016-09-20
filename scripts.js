39b62cb5b016128642ea7c84b538fdb5163cd67e
Denis <den.spirit@mail.ru>
2016-09-20 18:32:33 +0300

    playlist creating

--- scripts.js
+++ scripts.js
@@ -1,47 +1,52 @@
-(function($){
+$(document).ready(function() {
+    window.empty = $('.jp-type-single')[0].outerHTML;
+});
 
-    $(document).ready(function(){
 
-        $("#jplayer_viewport").jPlayer({
+function run(title, jpg, poster, lyrics) {
+    (function ($) {
+        $(".jp-type-single").replaceWith(window.empty);
+
+        $("#jplayer_player_1").jPlayer({
             ready: function (event) {
                 var self = this;
                 var $this = $(this);
                 $this.jPlayer("setMedia", {
-                    title: "Гацуурхан",
-                    mp3: "media/gatsuurhan.mp3",
-                    poster: "jplayer/viewport.jpg"
+                    title: title,
+                    mp3: jpg,
+                    poster: poster
                 });
                 $this.append($('<div id="karaoke_lyrics" data-index="-1"><span class="word">jPlayer, HTML, CSS3 Karaoke</span></div>'));
 
-                $.post( $this.jPlayer("option", "lyrics"), {}, function(data){
+                $.post($this.jPlayer("option", "lyrics"), {}, function (data) {
                     window.karaoke_stimes = [];
                     window.karaoke_etimes = [];
                     window.karaoke_current_index = -1;
                     window.karaoke = $(data);
 
-                    $(data).find('karaoke > add').each(function(index){
+                    $(data).find('karaoke > add').each(function (index) {
                         var st = $(this).attr('startTime');
                         var et = $(this).attr('endTime');
 
                         var sts = st.split(':');
-                        st = parseFloat(sts[0], 10)*60*60 + parseFloat(sts[1], 10)*60 + parseFloat(sts[2]);
-                        $(this).attr('startTime' , st);
+                        st = parseFloat(sts[0], 10) * 60 * 60 + parseFloat(sts[1], 10) * 60 + parseFloat(sts[2]);
+                        $(this).attr('startTime', st);
                         window.karaoke_stimes.push(st);
 
                         var ets = et.split(':');
-                        et = parseFloat(ets[0], 10)*60*60 + parseFloat(ets[1], 10)*60 + parseFloat(ets[2]);
-                        $(this).attr('endTime' , et);
+                        et = parseFloat(ets[0], 10) * 60 * 60 + parseFloat(ets[1], 10) * 60 + parseFloat(ets[2]);
+                        $(this).attr('endTime', et);
                         window.karaoke_etimes.push(et);
                     });
 
                     $this.jPlayer("play");
 
-                    setTimeout(function(){
+                    setTimeout(function () {
                         $('#karaoke_lyrics').html($(data).find('details singer').text() + ' - ' + $(data).find('details name').text());
                     }, 3000);
                 });
             },
-            lyrics: "media/gatsuurhan.xml",
+            lyrics: lyrics,
             swfPath: "jplayer/",
             supplied: "mp3",
             size: {
@@ -55,38 +60,40 @@
             keyEnabled: true,
             remainingDuration: true,
             toggleDuration: true,
-            timeupdate: function(event){
+            timeupdate: function (event) {
                 var $karaoke = window.karaoke;
                 var currentTime = event.jPlayer.status.currentTime;
-                if( $karaoke!==null && window.karaoke_stimes ){
-                    var md = parseInt(window.karaoke_stimes.length/2);
+                if ($karaoke !== null && window.karaoke_stimes) {
+                    var md = parseInt(window.karaoke_stimes.length / 2);
                     var si = 0;
-                    if( currentTime>window.karaoke_stimes[md] ){
+                    if (currentTime > window.karaoke_stimes[md]) {
                         si = md;
                         md = window.karaoke_stimes.length;
                     }
-                    else{
+                    else {
                         si = 0;
-                        md = md+1;
+                        md = md + 1;
                     }
 
-                    for( var i=si; i<md; i++ ){
-                        if( currentTime>=window.karaoke_stimes[i] && currentTime<=window.karaoke_etimes[i] && i>window.karaoke_current_index ){
+                    for (var i = si; i < md; i++) {
+                        if (currentTime >= window.karaoke_stimes[i] && currentTime <= window.karaoke_etimes[i] && i > window.karaoke_current_index) {
                             $('#karaoke_lyrics').html("");
                             var sumDuration = 0;
-                            $karaoke.find('karaoke > add').eq(i).find('entry').each(function(){
-                                var $layer = $('<span></span>').text( $(this).text() );
-                                var $text = $('<span class="word"></span>').text( $(this).text() ).append($layer);
-                                var duration = parseInt($(this).attr('duration'))/1000;
+                            $karaoke.find('karaoke > add').eq(i).find('entry').each(function () {
+                                var $layer = $('<span></span>').text($(this).text());
+                                var $text = $('<span class="word"></span>').text($(this).text()).append($layer);
+                                var duration = parseInt($(this).attr('duration')) / 1000;
                                 $text.css({
-                                    '-webkit-animation-duration': duration+"s",
-                                    '-moz-animation-duration': duration+"s",
-                                    '-ms-animation-duration': duration+"s",
-                                    '-o-animation-duration': duration+"s",
-                                    'animation-duration': duration+"s"
+                                    '-webkit-animation-duration': duration + "s",
+                                    '-moz-animation-duration': duration + "s",
+                                    '-ms-animation-duration': duration + "s",
+                                    '-o-animation-duration': duration + "s",
+                                    'animation-duration': duration + "s"
                                 });
-                                setTimeout(function(){$text.addClass('animate');}, sumDuration);
-                                sumDuration += duration*1000;
+                                setTimeout(function () {
+                                    $text.addClass('animate');
+                                }, sumDuration);
+                                sumDuration += duration * 1000;
                                 $('#karaoke_lyrics').append($text);
                             });
                             window.karaoke_current_index = i;
@@ -96,7 +103,5 @@
                 }
             }
         });
-
-    });
-
-})(jQuery);
+    })(jQuery);
+}
