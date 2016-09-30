50d5e96b03131f781dbf58a2aa2a3a5e4f243292
viacheslav <viacheslav.grygoriev@gmail.com>
2016-09-30 10:42:12 +0300

    new xml parsed

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    window.empty = $('.jp-type-single')[0].outerHTML;
+jQuery(document).ready(function() {
+    window.empty = jQuery('.jp-type-single')[0].outerHTML;
 
     // class "active" switcher
-    var listOfLis = $('.playlist-container .playlist-item');
+    var listOfLis = jQuery('.playlist-container .playlist-item');
     listOfLis.each(function(){
-       $(this).on("click",function(){
+       jQuery(this).on("click",function(){
            listOfLis.filter('li.active').removeClass('active');
-           $(this).addClass('active');
+           jQuery(this).addClass('active');
        });
     });
 
@@ -15,25 +15,25 @@ $(document).ready(function() {
 
 });
 
-// calculation of delay time minus 6 seconds for countdown screen saver
+// calculation of delay time for countdown screen saver
 var arrayOfCountdown = ['GO',1,2,3];
 function delayTime(){
-    var time = ($(karaoke).find('karaoke add:first-child').attr('startTime') - (arrayOfCountdown.length + 1)) * 1000;
+    var time = (jQuery(karaoke).find('karaoke add:first-child').attr('startTime') - (arrayOfCountdown.length + 1)) * 1000;
     return Math.round(time);
 }
 // start screen saver with proper delay
 function startSong(timeBefore){
     clearTimeout(window.timeoutForCountdown);
         window.timeoutForCountdown = setTimeout(function(){
-        new Countdown($('#jplayer_player_1'));
+        new Countdown(jQuery('#jplayer_player_1'));
     }, timeBefore);
 }
 // Object for countdown
 function Countdown(parentNode){
     this.parentNode = parentNode;
     this.countdownData = arrayOfCountdown;
-    this.background = $('<div class="countdown-bg">');
-    this.coundownNumbers = $('<div class="wrapper-for-numbers"><span class="numbers"></span></div>');
+    this.background = jQuery('<div class="countdown-bg">');
+    this.coundownNumbers = jQuery('<div class="wrapper-for-numbers"><span class="numbers"></span></div>');
     this.interval = null;
     this.timeout = null;
     this._buildHTML(this.parentNode);
@@ -45,7 +45,7 @@ Countdown.prototype._buildHTML = function (parentNode) {
 };
 Countdown.prototype._numbersIterate = function () {
     var _this = this;
-    var num = $('.numbers');
+    var num = jQuery('.numbers');
     this.interval = setInterval(function(){
         if (_this.countdownData.length === 0){
             _this._destroyHTML(_this.parentNode);
@@ -59,51 +59,43 @@ Countdown.prototype._numbersIterate = function () {
     }, 1000);
 };
 Countdown.prototype._destroyHTML = function (parentNode){
-    $(parentNode).find($(".countdown-bg")).remove();
-    $(parentNode).find($('.wrapper-for-numbers')).remove();
+    jQuery(parentNode).find(jQuery(".countdown-bg")).remove();
+    jQuery(parentNode).find(jQuery('.wrapper-for-numbers')).remove();
 };
 
 function run(title, jpg, poster, lyrics) {
     (function ($) {
-        $(".jp-type-single").replaceWith(window.empty);
+        jQuery(".jp-type-single").replaceWith(window.empty);
 
-        $("#jplayer_player_1").jPlayer({
+        jQuery("#jplayer_player_1").jPlayer({
             ready: function (event) {
+                window.flag = true;
                 var self = this;
-                var $this = $(this);
+                var $this = jQuery(this);
                 $this.jPlayer("setMedia", {
                     title: title,
                     mp3: jpg,
                     poster: poster
                 });
-                $this.append($('<div id="karaoke_lyrics" data-index="-1"><span class="word">jPlayer, HTML, CSS3 Karaoke</span></div>'));
+                $this.append(jQuery('<div id="karaoke_lyrics" data-index="-1"><span class="word">jPlayer, HTML, CSS3 Karaoke</span></div>'));
 
                 $.post($this.jPlayer("option", "lyrics"), {}, function (data) {
-                    window.karaoke_stimes = [];
-                    window.karaoke_etimes = [];
+                    window.karaoke_words = {};
                     window.karaoke_current_index = -1;
-                    window.karaoke = $(data);
+                    window.karaoke = jQuery(data);
 
+                    jQuery(data).find('word').each(function (index) {
+                        var word = jQuery(this).text().trim();
+                        var time = jQuery(this).attr('position');
+                        var color = jQuery(this).attr('color');
+                        window.karaoke_words[index] = {word :word, color: color, time: time};
 
-                    $(data).find('karaoke > add').each(function (index) {
-                        var st = $(this).attr('startTime');
-                        var et = $(this).attr('endTime');
-
-                        var sts = st.split(':');
-                        st = parseFloat(sts[0]) * 60 * 60 + parseFloat(sts[1]) * 60 + parseFloat(sts[2]);
-                        $(this).attr('startTime', st);
-                        window.karaoke_stimes.push(st);
-
-                        var ets = et.split(':');
-                        et = parseFloat(ets[0]) * 60 * 60 + parseFloat(ets[1]) * 60 + parseFloat(ets[2]);
-                        $(this).attr('endTime', et);
-                        window.karaoke_etimes.push(et);
                     });
 
                     $this.jPlayer("play");
                     startSong(delayTime());
                     setTimeout(function () {
-                        $('#karaoke_lyrics').html($(data).find('details singer').text() + ' - ' + $(data).find('details name').text());
+                        jQuery('#karaoke_lyrics').html(jQuery(data).find('details singer').text() + ' - ' + jQuery(data).find('details name').text());
                     }, 3000);
                 });
             },
@@ -123,38 +115,50 @@ function run(title, jpg, poster, lyrics) {
             toggleDuration: true,
             timeupdate: function (event) {
                 var $karaoke = window.karaoke;
-                var currentTime = event.jPlayer.status.currentTime;
+                var currentTime = event.jPlayer.status.currentTime.toFixed(3);
                 if ($karaoke !== null && window.karaoke_stimes) {
                     var md = parseInt(window.karaoke_stimes.length);
                     for (var i = 0; i < md; i++) {
                         (function(i){
+                            console.log(window.flag);
+
                             if (currentTime >= window.karaoke_etimes[i-1] && currentTime <= window.karaoke_stimes[i]) {
-                                var entry = $karaoke.find('karaoke > add').eq(i).find('entry').text();
-                                $('#karaoke_lyrics').html("");
-                                var text = $('<span class="word"></span>');
-                                entry.each(function(){
-                                    text.append($(this).text() + " ")
+                                window.flag = true;
+                                jQuery('#karaoke_lyrics').html("");
+                                $karaoke.find('karaoke > add').eq(i).find('entry').each(function(){
+                                    var $layer = jQuery('<span></span>').text( jQuery(this).text() );
+                                    var $text = jQuery('<span class="word"></span>').text( jQuery(this).text() ).append($layer);
+                                    jQuery('#karaoke_lyrics').append($text);
                                 });
-                                $('#karaoke_lyrics').append(text);
-                            } else if (currentTime >= window.karaoke_stimes[i] && currentTime <= window.karaoke_etimes[i]) {
-                                debugger
-                                var entry = $karaoke.find('karaoke > add').eq(i).find('entry');
-                                $('#karaoke_lyrics').html("");
-                                var text = $('<span class="word"></span>');
-                                entry.each(function(){
-                                    text.append($(this).text() + " ")
+                            } else if (currentTime >= window.karaoke_stimes[i] && currentTime <= window.karaoke_etimes[i] && window.flag) {
+                                window.flag = false;
+                                jQuery('#karaoke_lyrics').html("");
+                                var sumDuration = 0;
+                                $karaoke.find('karaoke > add').eq(i).find('entry').each(function(){
+                                    var $layer = jQuery('<span></span>').text( jQuery(this).text() );
+                                    var $text = jQuery('<span class="word"></span>').text( jQuery(this).text() ).append($layer);
+                                    var duration = parseInt(jQuery(this).attr('duration'))/1000;
+                                    $text.css({
+                                        '-webkit-animation-duration': duration+"s",
+                                        '-moz-animation-duration': duration+"s",
+                                        '-ms-animation-duration': duration+"s",
+                                        '-o-animation-duration': duration+"s",
+                                        'animation-duration': duration+"s"
+                                    });
+                                    setTimeout(function(){$text.addClass('animate');}, sumDuration);
+                                    sumDuration += duration*1000;
+                                    jQuery('#karaoke_lyrics').append($text);
                                 });
-                                $('#karaoke_lyrics').append(text);
                             } else if (currentTime >= window.karaoke_etimes[i] && currentTime <= window.karaoke_stimes[i+1]) {
-                                var entry = $karaoke.find('karaoke > add').eq(i+1).find('entry').text();
-                                $('#karaoke_lyrics').html("");
-                                var text = $('<span class="word"></span>');
-                                entry.each(function(){
-                                    text.append($(this).text() + " ")
+                                window.flag = true;
+                                jQuery('#karaoke_lyrics').html("");
+                                $karaoke.find('karaoke > add').eq(i+1).find('entry').each(function(){
+                                    var $layer = jQuery('<span></span>').text( jQuery(this).text() );
+                                    var $text = jQuery('<span class="word"></span>').text( jQuery(this).text() ).append($layer);
+                                    jQuery('#karaoke_lyrics').append($text);
                                 });
-                                $('#karaoke_lyrics').append(text);
                             } else if (currentTime >= window.karaoke_etimes[i] && window.karaoke_stimes[i+1] === undefined) {
-                                $('#karaoke_lyrics').html("");
+                                jQuery('#karaoke_lyrics').html("");
                             }
                         })(i);
                     }
